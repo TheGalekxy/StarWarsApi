863a28b83bacfeae0e8a1ecea09326faf217ac37
Adam Galek <galekxy13@hotmail.com>
2020-03-01 11:52:52 -0500

    build

---
+++ starwars-api-app/build/precache-manifest.fba27793fcaf580f4517ca36de92a782.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e21e952fb804df981f3889ab76ee867",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2913d5032f80db63d59c",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "d9ae6e69af27abef6fa7",
+    "url": "/static/css/main.82142c7d.chunk.css"
+  },
+  {
+    "revision": "2913d5032f80db63d59c",
+    "url": "/static/js/2.86317df3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.86317df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9ae6e69af27abef6fa7",
+    "url": "/static/js/main.2cbf093f.chunk.js"
+  },
+  {
+    "revision": "87afe1b1e46d55043e7b",
+    "url": "/static/js/runtime-main.5ef29927.js"
+  },
+  {
+    "revision": "7fc03b20dba6644d7f2c8adfb58a2616",
+    "url": "/static/media/STJEDISE.7fc03b20.woff"
+  },
+  {
+    "revision": "5c345b90c45c6fad10386d96cc93a030",
+    "url": "/static/media/Starjedi.5c345b90.woff"
+  },
+  {
+    "revision": "b5777b8f88fb6c2cce165434e74d6270",
+    "url": "/static/media/Starjhol.b5777b8f.woff"
+  },
+  {
+    "revision": "f359094b1bb10bac9417b4445bc792ac",
+    "url": "/static/media/Starjout.f359094b.woff"
+  },
+  {
+    "revision": "bd2c1fbb53a7b96992f7fcb37b6b14a9",
+    "url": "/static/media/Stjldbl1.bd2c1fbb.woff"
+  },
+  {
+    "revision": "3994222dc74b0fa1ab81d1198ae7d015",
+    "url": "/static/media/Stjldbl2.3994222d.woff"
+  },
+  {
+    "revision": "bb3fdf4f93a5d8ccd4df686d74a4b206",
+    "url": "/static/media/Strjmono.bb3fdf4f.woff"
+  }
+]);
