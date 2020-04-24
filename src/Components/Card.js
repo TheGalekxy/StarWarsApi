dd3c7bcc5be0bddbadfc93e96018be90ad630d46
Adam <galekxy13@hotmail.com>
2020-04-24 19:12:01 -0400

    github pages build

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,7 +4,7 @@ const Card = ({name = 'unknown', gender = 'n/a', weight = 'unknown', species = '
     
     return (
         <div className='tc bg-moon-gray dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img src={`https://robohash.org/${species}2?200x200`} alt='species'/> {/* Added the '2' after the species variable in-case there is no species and hence an empty array */}
+            <img src={`https://robohash.org/${species}2`} alt='species'/> {/* Added the '2' after the species variable in-case there is no species and hence an empty array */}
             <div>
                 <h2> {name} </h2>
                 <p> Gender: {gender} </p>
